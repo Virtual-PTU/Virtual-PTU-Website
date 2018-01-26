e7f49a8c2c68f7507950653960acfe0e049799de
Zach Barham <me@zoweb.me>
2018-01-26 15:51:19 +1000

    Add logic for footer

---
+++ beta/js/footer.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2018, zoweb
+ *
+ * See the license in the LICENSE file (downloaded with this repository, in the root folder)
+ * By using this code, you agree to the license in the file specified (the MIT license)
+ */
+
+(function() {
+    document.querySelector("#copyright-year").textContent = new Date().getFullYear();
+}());
