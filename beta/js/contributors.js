014842fbe2dc796e8ac391df4bc023aa67ddd1c8
Zach Barham <me@zoweb.me>
2018-01-26 15:51:59 +1000

    Make credits use data from Github

---
+++ beta/js/contributors.js
@@ -0,0 +1,25 @@
+function initContributors(users) {
+    const $credits = document.querySelector(".credits"),
+        $template = document.getElementById("credit-template");
+
+    users.forEach(user => {
+        $template.content.querySelector(".username").textContent = `${user.username}` + (user.firstName ? ` (${user.firstName})` : "");
+
+        $template.content.querySelector(".short-description").textContent = user.shortDescription;
+        $template.content.querySelector(".tags").innerHTML = user.tags.map(tag => `<span class="tag ${tag}"></span>`).join(" ");
+
+        let linksHtml = "";
+        if (user.links.github) linksHtml += `<a href="https://github.com/${user.links.github}" class="link github"></a> `;
+        if (user.links.ptForum) linksHtml += `<a href="http://forums.pokemontabletop.com/profile/${user.links.ptForum}" class="link forums"></a> `;
+        if (user.links.youtube) linksHtml += `<a href="https://youtube.com/${user.links.youtube}" class="link youtube"></a>`;
+        $template.content.querySelector(".links").innerHTML = linksHtml;
+
+        const $clone = document.importNode($template.content, true);
+        const $credit = document.createElement("section");
+        $credit.className = "credit";
+        $credit.appendChild($clone);
+        $credits.appendChild($credit);
+    });
+
+    $template.remove();
+}
