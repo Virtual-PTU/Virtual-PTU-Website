c783e43b65c9fbb507016419f3568402a1a2a87b
Zach Barham <me@zoweb.me>
2018-01-27 18:21:49 +1000

    Add alerts

---
+++ beta/js/auth.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyCaaDRWfqD11-y1xgQz28ty-ouw4N6FOGA",
+    authDomain: "vptu-b790b.firebaseapp.com",
+    databaseURL: "https://vptu-b790b.firebaseio.com",
+    projectId: "vptu-b790b",
+    storageBucket: "vptu-b790b.appspot.com",
+    messagingSenderId: "411319664302"
+});
